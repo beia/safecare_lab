06bd4e2f73e3e464e23611f19fde157e67c383eb
Eduard101 <ediepurica@yahoo.com>
2020-10-09 13:30:20 +0300

    Changed Event name for Fire sensor

--- BeiaDeviceDriver_Fire/DeviceCommunication/InputPoller.cs
+++ BeiaDeviceDriver_Fire/DeviceCommunication/InputPoller.cs
@@ -79,7 +79,7 @@ namespace Safecare.BeiaDeviceDriver_Fire
 
                         // Send an XProtect event
                         _eventManager.NewEvent(Constants.DriverId.ToString(), EventId.OutputChanged,
-                            new System.Collections.Generic.Dictionary<string, string> { { "Thermometer", msg } });
+                            new System.Collections.Generic.Dictionary<string, string> { { "Fire Detection", msg } });
 
                         _lastUpdateTime = _messageHandler.Data.Time;
                     }
